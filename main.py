e1a0f308d29cb186dd97bfe69a10dd6fedd442ac
PsorTheDoctor <psor2.0@gmail.com>
2024-10-19 23:08:59 +0200

    func splitted

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ import clip
 from global_torch.manipulate import Manipulator
 from global_torch.StyleCLIP import GetDt, GetBoundary
 
+dataset_name = 'ffhq'
+experiment_type = 'ffhq_encode'
+
+EXPERIMENT_ARGS = {
+    "model_path": "e4e_ffhq_encode.pt"
+}
+EXPERIMENT_ARGS['transform'] = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
 
 def run_alignment(image_path):
     import dlib
@@ -30,18 +41,7 @@ def run_on_batch(inputs, net):
     return images, latents
 
 
-def main(image_path, target, neutral='a face', alpha=4.1, beta=0.15):
-    """
-    :param image_path: a path to the given image
-    :param target: a prompt describing desired modifications
-    :param neutral: a prompt describing the input image
-    :param alpha: manipulation strength, ranges (-10, 10)
-    :param beta: disentanglement threshold, ranges (0.08 - 0.3)
-    :return: a modified PIL image
-    """
-    dataset_name = 'ffhq'
-    experiment_type = 'ffhq_encode'
-
+def load_model():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
 
@@ -53,34 +53,38 @@ def main(image_path, target, neutral='a face', alpha=4.1, beta=0.15):
     M.G = G
     M.SetGParameters()
     num_img = 100_000
-    M.GenerateS(num_img = num_img)
+    M.GenerateS(num_img=num_img)
     M.GetCodeMS()
-    np.set_printoptions(suppress = True)
-
-    file_path = './npy/' + dataset_name + '/'
-    fs3 = np.load(file_path + 'fs3.npy')
+    np.set_printoptions(suppress=True)
 
     """
     encoder4editing setup
     """
-    EXPERIMENT_ARGS = {
-            "model_path": "e4e_ffhq_encode.pt"
-        }
-    EXPERIMENT_ARGS['transform'] = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-    resize_dims = (256, 256)
-
     model_path = EXPERIMENT_ARGS['model_path']
     ckpt = torch.load(model_path, map_location='cpu')
     opts = ckpt['opts']
     opts['checkpoint_path'] = model_path
-    opts= Namespace(**opts)
+    opts = Namespace(**opts)
     net = pSp(opts)
     net.eval()
     net.cuda()
     print('Model successfully loaded!')
+    return model, M
+
+
+def main(image_path, model, manipulator, target, neutral='a face',
+         alpha=4.1, beta=0.15, resize_dims=(256, 256)):
+    """
+    :param model: CLIP ViT model
+    :param manipulator: custom image manipulator
+    :param image_path: a path to the given image
+    :param target: a prompt describing desired modifications
+    :param neutral: a prompt describing the input image
+    :param alpha: manipulation strength, ranges (-10, 10)
+    :param beta: disentanglement threshold, ranges (0.08 - 0.3)
+    :return: a modified PIL image
+    """
+    M = manipulator
 
     original_image = Image.open(image_path)
     original_image = original_image.convert("RGB")
@@ -124,9 +128,18 @@ def main(image_path, target, neutral='a face', alpha=4.1, beta=0.15):
     classnames = [target, neutral]
     dt = GetDt(classnames, model)
 
+    file_path = './npy/' + dataset_name + '/'
+    fs3 = np.load(file_path + 'fs3.npy')
+
     M.alpha = [alpha]
     boundary_tmp2, c = GetBoundary(fs3, dt, M, threshold=beta)
     codes = M.MSCode(dlatent_tmp, boundary_tmp2)
     out = M.GenerateImg(codes)
     generated = Image.fromarray(out[0, 0])
     return generated
+
+
+# Example usage
+# if __name__ == '__main__':
+    # model, manipulator = load_model()
+    # main('adam.jpg', model, manipulator, target='a grumpy face')
